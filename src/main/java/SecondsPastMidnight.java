1d40820e44790b8b7e3054be0a1bf563ec1692e8
William Giraldo <william.229937@nv.ccsd.net>
2020-09-04 21:57:07 +0000

    Finish the SecondsPastMidnight Class.

--- src/main/java/SecondsPastMidnight.java
+++ src/main/java/SecondsPastMidnight.java
@@ -7,8 +7,14 @@ class SecondsPastMidnight {
         Scanner scanner = new Scanner(System.in);
 
         //Reads int from user
+       
         int number = scanner.nextInt();
+        int hours;
+        int mins;
+        //Your code goes here
 
+       hours = number / 3600;
+       mins =  number / 60;
         //Your code goes here
 
 
